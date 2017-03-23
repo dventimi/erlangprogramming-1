6ab6eec803ab6544ae4f5bc48b64f8f32cddc434
David A. Ventimiglia <dventimi@gmail.com>
2017-03-23 14:34:12 -0700

    Minor formatting changes.

--- chapter5/io_handler.erl
+++ chapter5/io_handler.erl
@@ -14,24 +14,24 @@ init(Count) -> Count.
 terminate(Count) -> {count, Count}.
 
 handle_event({raise_alarm, Id, Alarm}, Count) ->
-  print(alarm, Id, Alarm, Count),
-  Count+1;
+    print(alarm, Id, Alarm, Count),
+    Count+1;
 handle_event({clear_alarm, Id, Alarm}, Count) ->
-  print(clear, Id, Alarm, Count),
-  Count+1;
-handle_event(Event, Count) ->
-  Count.
+    print(clear, Id, Alarm, Count),
+    Count+1;
+handle_event(_Event, Count) ->
+    Count.
 
 print(Type, Id, Alarm, Count) ->
-  Date = fmt(date()), Time = fmt(time()),
-  io:format("#~w,~s,~s,~w,~w,~p~n",
-            [Count, Date, Time, Type, Id, Alarm]).
+    Date = fmt(date()), Time = fmt(time()),
+    io:format("#~w,~s,~s,~w,~w,~p~n",
+	      [Count, Date, Time, Type, Id, Alarm]).
 
 fmt({AInt,BInt,CInt}) ->
-  AStr = pad(integer_to_list(AInt)),
-  BStr = pad(integer_to_list(BInt)),
-  CStr = pad(integer_to_list(CInt)),
-  [AStr,$:,BStr,$:,CStr].
+    AStr = pad(integer_to_list(AInt)),
+    BStr = pad(integer_to_list(BInt)),
+    CStr = pad(integer_to_list(CInt)),
+    [AStr,$:,BStr,$:,CStr].
 
 pad([M1])  -> [$0,M1];
 pad(Other) -> Other.
--- chapter5/log_handler.erl
+++ chapter5/log_handler.erl
@@ -10,15 +10,15 @@
 -export([init/1, terminate/1, handle_event/2]).
 
 init(File) ->
-  {ok, Fd} = file:open(File, write),
-  Fd.
+    {ok, Fd} = file:open(File, write),
+    Fd.
 
 terminate(Fd) -> file:close(Fd).
 
 handle_event({Action, Id, Event}, Fd) ->
-  {MegaSec, Sec, MicroSec} = now(),
-  Args = io:format(Fd, "~w,~w,~w,~w,~w,~p~n",
-                   [MegaSec, Sec, MicroSec, Action, Id, Event]),
-  Fd;
+    {MegaSec, Sec, MicroSec} = now(),
+    _Args = io:format(Fd, "~w,~w,~w,~w,~w,~p~n",
+		      [MegaSec, Sec, MicroSec, Action, Id, Event]),
+    Fd;
 handle_event(_, Fd) ->
-  Fd.
+    Fd.
