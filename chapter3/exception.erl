5c7101e2fc56d21d00ab0f643a69e95d7e91e57c
David A. Ventimiglia <dventimi@gmail.com>
2017-03-29 10:52:17 -0700

    Summary: Minor formatting changes

--- chapter3/exception.erl
+++ chapter3/exception.erl
@@ -10,49 +10,49 @@
 -export([return_error/1,try_return/1,try_wildcard/1,try_return2/1,return/1]).
 
 return_error(X) when X < 0 ->
-  throw({'EXIT', {badarith,
-                 [{exception,return_error,1},
-                  {erl_eval,do_apply,5},
-                  {shell,exprs,6},
-                  {shell,eval_exprs,6},
-                  {shell,eval_loop,3}]}});
+    throw({'EXIT', {badarith,
+		    [{exception,return_error,1},
+		     {erl_eval,do_apply,5},
+		     {shell,exprs,6},
+		     {shell,eval_exprs,6},
+		     {shell,eval_loop,3}]}});
 return_error(X) when X == 0 ->
-  1/X;
+    1/X;
 return_error(X) when X > 0->
-  {'EXIT', {badarith, [{exception,return_error,1},
-                       {erl_eval,do_apply,5},
-                       {shell,exprs,6},
-                       {shell,eval_exprs,6},
-                       {shell,eval_loop,3}]}}.
+    {'EXIT', {badarith, [{exception,return_error,1},
+			 {erl_eval,do_apply,5},
+			 {shell,exprs,6},
+			 {shell,eval_exprs,6},
+			 {shell,eval_loop,3}]}}.
 
 try_return(X) when is_integer(X) -> 
-  try return_error(X) of 
-    Val -> {normal, Val} 
-  catch 
-    exit:Reason -> {exit, Reason}; 
-    throw:Throw -> {throw, Throw}; 
-    error:Error -> {error, Error} 
-  end. 
+    try return_error(X) of 
+	Val -> {normal, Val} 
+    catch 
+	exit:Reason -> {exit, Reason}; 
+	throw:Throw -> {throw, Throw}; 
+	error:Error -> {error, Error} 
+    end. 
 
 try_wildcard(X) when is_integer(X) ->
-  try return_error(X)
+    try return_error(X)
     catch
-    throw:Throw -> {throw, Throw};
-    error:_     -> error;
-    Type:Error  -> {Type, Error};
-    _           -> other;                 %% Will never be returned
-    _:_         -> other                  %% Will never be returned
-  end.
+	throw:Throw -> {throw, Throw};
+	error:_     -> error;
+	Type:Error  -> {Type, Error};
+	_           -> other;                 %% Will never be returned
+	_:_         -> other                  %% Will never be returned
+    end.
 
 try_return2(X) when is_integer(X) ->
-  try return_error(X) of
-    Val -> {normal, Val}
+    try return_error(X) of
+	Val -> {normal, Val}
     catch
-    exit:_  -> 34;
-    throw:_ -> 99;
-    error:_ -> 678
-  end.
+	exit:_  -> 34;
+	throw:_ -> 99;
+	error:_ -> 678
+    end.
 
 return(X) when is_integer(X) ->
-  catch return_error(X).
+    catch return_error(X).
 
--- chapter3/math.erl
+++ chapter3/math.erl
@@ -10,9 +10,9 @@
 -export([add/2]).
 
 add(X,Y) ->
-  test_int(X),
-  test_int(Y),
-  X + Y.
+    test_int(X),
+    test_int(Y),
+    X + Y.
 
 test_int(Int) when is_integer(Int) -> true;
 test_int(Int) -> throw({error, {non_integer, Int}}).
